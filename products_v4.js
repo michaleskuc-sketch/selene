ddf1e91d869b1127a024e397c5f3a0cf2d6efd35
michaleskuc-sketch <michaleskuc@gmail.com>
2026-08-27 02:08:08 +0200

    Produkty: +SEL-AN-01242 (Studio/Detail/Modelka 1+2/Mockup), galeria s oboma modelka fotkami (5 nahladov)

--- products_v4.js
+++ products_v4.js
@@ -4,5 +4,6 @@ var COLLS = {"sun-gold": "Sun Gold", "moon-silver": "Moon Silver"};
 var MATS = {"striebro": "Striebro 925", "ocel": "Chirurgická oceľ", "pozlacene": "Pozlátené striebro"};
 var PLATS = {"zlata": "Zlatá", "rodio": "Ródiovaná", "bez": "Bez pokovenia"};
 var PRODUCTS = [
-  {id:0, n:'Nastaviteľný prsteň s kvetom a ružovými zirkónmi', t:'prstene', m:'ocel', pl:'zlata', s:'zirkon', k:'sun-gold', c:11.90, img:'img/SEL-RI-9F4BA.jpg', sku:'SEL-RI-9F4BA', b:'novinka', gal:['img/SEL-RI-9F4BA.jpg','img/SEL-RI-9F4BA_detail.jpg','img/SEL-RI-9F4BA_modelka_2.jpg','img/SEL-RI-9F4BA_mockup.jpg']},
+  {id:0, n:'Nastaviteľný prsteň s kvetom a ružovými zirkónmi', t:'prstene', m:'ocel', pl:'zlata', s:'zirkon', k:'sun-gold', c:11.9, img:'img/SEL-RI-9F4BA.jpg', sku:'SEL-RI-9F4BA', b:'novinka', gal:["img/SEL-RI-9F4BA.jpg", "img/SEL-RI-9F4BA_detail.jpg", "img/SEL-RI-9F4BA_modelka_2.jpg", "img/SEL-RI-9F4BA_modelka_1.jpg", "img/SEL-RI-9F4BA_mockup.jpg"]},
+  {id:1, n:'Retiazka na nohu 23 cm, dvojradová so zirkónmi', t:'anklety', m:'ocel', pl:'zlata', s:'zirkon', k:'sun-gold', c:17.95, img:'img/SEL-AN-01242.jpg', sku:'SEL-AN-01242', b:'novinka', gal:["img/SEL-AN-01242.jpg", "img/SEL-AN-01242_detail.jpg", "img/SEL-AN-01242_modelka_2.jpg", "img/SEL-AN-01242_modelka_1.jpg", "img/SEL-AN-01242_mockup.jpg"]},
 ];
